dfba2bcabf626e685b932b4a5e9cd462db838e60
tylerestes <tyler.estes@students.fhu.edu>
2016-05-07 14:34:30 -0500

    Initial Commit

---
+++ fhu-socialclub-app/fhu-socialclub-app/js/controllers/app-controller.js
@@ -0,0 +1,34 @@
+﻿app.controller("AppController", ["$scope", "$location", '$window', function ($scope, $location, $window) {
+
+    $scope.reloadView = function () {
+        $window.location.reload();
+    }
+
+    $scope.username = "Jesse";
+    $scope.announcements = {
+        "general-announcements": {
+            "name": "Announcements",
+            "items": [
+        "Clubs are opening for recruitment this week.",
+        "First mixer events are scheduled for tomorrow evening at 8pm. Come prepared to get your attendance recorded by each club!",
+        "Registration and Declaration of Majors for Freshmen is available until this Friday."
+            ]
+        },
+        "club-announcements": {
+            "name": "Club Activities",
+            "items": [
+                    "XBX Chickasaw Retreat",
+                    "ON Friendship Picnic",
+                    "EP Rumble Rally"
+            ]
+        },
+        "sports-announcements": {
+            "name": "Sports",
+            "items": [
+                    "Mon Apr 22nd | Softball - PKA Bad Boys vs. XBX Eagles",
+                    "Tues Apr 23rd | Volleyball - XCD Titans vs. GTO Marauders",
+                    ""
+            ]
+        }
+    };
+}]);
---
+++ fhu-socialclub-app/fhu-socialclub-app/js/routes.js
@@ -0,0 +1,13 @@
+﻿app.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: '/views/dashboard.html',
+            controller: 'AppController'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+
+    $locationProvider.html5Mode(true);
+
+});
